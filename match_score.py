ae436154be6f7a3c82442cd69b804cb8ed4396b3
Victor Liu <victorliu1231@gmail.com>
2020-07-23 14:50:45 -0400

    added matching by artist and matching by genre functions

--- match_score.py
+++ match_score.py
@@ -1,4 +1,8 @@
+#compares two users' playlists and outputs a "match score" that determines how similar are their playlists. A higher score means their playlists are more similar.
+#the same songs have a higher weight in determining score, followed by the same artist, followed by the same genre
+#the function takes in two lists of dictionaries named "playlist1" and "playlist2"
 #playlist1 should be the user's playlist and playlist2 should be the playlist of whoever the user is trying to match with
+#returns the two users' match scores
 def match_score_by_song(p1, p2):
     #songs
     global song_score
@@ -57,6 +61,96 @@ def match_score_by_song(p1, p2):
     
     return total_score
 
+#does the same thing as match_score_by_song except we don't take into account similar songs at all now in matchmaking, only similar artists and genres
+def match_score_by_artist(p1, p2):
+    global song_score
+    song_score = 0
+    
+    #artists
+    #creates a dictionary of the artists in playlist1 and playlist2 to remove duplicate artists in entries
+    global artist_score
+    artist_score = 0
+    p1_artists = {}
+    for song in p1:
+        p1_artists[song["artist"]] = "any_value"
+
+    p2_artists = {}
+    for song in p2:
+        p2_artists[song["artist"]] = "any_value"
+
+    #loop through p1_artists and see if the artist in playlist1 can be located in playlist2. If so, add a score of 2.
+    for artist in p1_artists:
+        if artist in p2_artists:
+            artist_score+= 2
+
+
+    #genres
+    #creates a dictionary of the genres in playlist1 and playlist2 to remove duplicate genres in entries
+    global genre_score
+    genre_score = 0
+    p1_genres = {}
+    for song in p1:
+        p1_genres[song["genre"]] = "any_value"
+
+    p2_genres = {}
+    for song in p2:
+        p2_genres[song["genre"]] = "any_value"
+
+    #loop through p1_genres and see if the genre in playlist1 can be located in playlist2. If so, add a score of 1.
+    for genre in p1_genres:
+        if genre in p2_genres:
+            genre_score+= 1
+
+    
+    #calculates maximum match score for the user, max_score should always be >= total_score
+    global max_score
+    max_score = 2 * len(p1_artists) + 1 * len(p1_genres)
+    
+    global total_score
+    total_score = artist_score + genre_score
+
+    if (max_score < total_score):
+        return "Error: Max score is somehow lower than total score, which shouldn't happen."
+    
+    return total_score
+
+#does the same thing as match_score_by_song except we don't take into account similar songs or similar artists at all now in matchmaking, only similar genres
+def match_score_by_genre(p1, p2):
+    global song_score
+    song_score = 0
+    global artist_score
+    artist_score = 0
+    
+    #genres
+    #creates a dictionary of the genres in playlist1 and playlist2 to remove duplicate genres in entries
+    global genre_score
+    genre_score = 0
+    p1_genres = {}
+    for song in p1:
+        p1_genres[song["genre"]] = "any_value"
+
+    p2_genres = {}
+    for song in p2:
+        p2_genres[song["genre"]] = "any_value"
+
+    #loop through p1_genres and see if the genre in playlist1 can be located in playlist2. If so, add a score of 1.
+    for genre in p1_genres:
+        if genre in p2_genres:
+            genre_score+= 1
+
+    
+    #calculates maximum match score for the user, max_score should always be >= total_score
+    global max_score
+    max_score = 1 * len(p1_genres)
+    
+    global total_score
+    total_score = genre_score
+
+    if (max_score < total_score):
+        return "Error: Max score is somehow lower than total score, which shouldn't happen."
+    
+    return total_score
+
 #returns match_percentage in percent form
 def match_percentage(total_score, max_score):
     return str(round(total_score / max_score * 100, 2)) + "%"
@@ -73,22 +167,38 @@ def genre_percentage(genre_score, total_score, match_percentage):
     return str(round(genre_score / max_score * 100, 2)) + "%"
 
 
-#test cases
-# p1 = [
-#     {"name":"Red", "artist": "Taylor Swift", "genre":"idk"},
-#     {"name":"Cruel Summer", "artist": "Taylor Swift", "genre": "idk"}
-# ]
+# #test cases
+p1 = [
+    {"name":"Red", "artist": "Taylor Swift", "genre":"idk"},
+    {"name":"Cruel Summer", "artist": "Taylor Swift", "genre": "idk"}
+]
 
-# p2 = [
-#     {"name":"Red", "artist": "Taylor Swift", "genre":"idk"},
-#     {"name":"Style", "artist": "Taylor Swift", "genre":"idk"},
-#     {"name":"Wildest Dreams", "artist": "Taylor Swift", "genre":"idk"},
-#     {"name":"Paradise", "artist": "Bazzi", "genre":"idk"}
-# ]
+p2 = [
+    {"name":"Red", "artist": "Taylor Swift", "genre":"idk"},
+    {"name":"Style", "artist": "Taylor Swift", "genre":"idk"},
+    {"name":"Wildest Dreams", "artist": "Taylor Swift", "genre":"idk"},
+    {"name":"Paradise", "artist": "Bazzi", "genre":"idk"},
+    {"name":"Naturally", "artist": "Selena Gomez", "genre": "pop"},
+    {"name":"prom dress", "artist":"mxmtoon","genre":"smth diff"},
+    {"name":"Party In The USA","artist":"Miley Cyrus","genre":"party"},
+    {"name":"Super Freak", "artist":"Rick James","genre":"disco"}
+]
 
-# print(match_score_by_song(p2, p1)) #should output 6
+p3 = [
+    {"name":"A Thousand Years of Rain", "artist": "Selena Gomez", "genre": "pop"}
+]
+
+# print(match_score_by_genre(p1,p2))
 # match_percentage = match_percentage(total_score, max_score) 
-# print(match_percentage) 
+# print(match_percentage)
+# print(song_percentage(song_score, total_score, match_percentage)) 
+# print(artist_percentage(artist_score, total_score, match_percentage))
+# print(genre_percentage(genre_score, total_score, match_percentage))
+
+# print(match_score_by_song(p1, p2))
+# match_percentage = match_percentage(total_score, max_score) 
+# print(match_percentage)
 # print(song_percentage(song_score, total_score, match_percentage)) 
 # print(artist_percentage(artist_score, total_score, match_percentage)) 
-# print(genre_percentage(genre_score, total_score, match_percentage)) 
+# print(genre_percentage(genre_score, total_score, match_percentage))
+
